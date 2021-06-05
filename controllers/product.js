c02f310d7184c72165a85a059c008d83f94a5ee4
Mohammad Amin <mohammadazimi16@gmail.com>
2021-06-05 19:00:35 +0430

    frontend

--- controllers/product.js
+++ controllers/product.js
@@ -48,7 +48,7 @@ module.exports = {
     return res.json(products);
   },
   popular: async (req, res) => {
-    let popular = await Product.find({}).sort("view");
+    let popular = await Product.find({}).sort({ view: -1 });
     res.send(popular);
   },
   getOne: async (req, res) => {

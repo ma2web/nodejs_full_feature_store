bb8976689df8c059b9e675ed15b35b0d22421e0e
Mohammad Amin <mohammadazimi16@gmail.com>
2021-06-16 19:22:22 +0430

    casback delete

--- views/_next/static/chunks/webpack.js
+++ views/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3a7dbb2db7b4b57ea3e9";
+/******/ 	var hotCurrentHash = "b207c2d7edaab68a83f1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

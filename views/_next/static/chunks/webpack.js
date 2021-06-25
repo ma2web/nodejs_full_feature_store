0486fd341aec795fa43193934dba716796945606
Mohammad Amin <mohammadazimi16@gmail.com>
2021-06-25 14:05:39 +0430

    home - home and post headers

--- views/_next/static/chunks/webpack.js
+++ views/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "239fb6a2ed2bfec130a0";
+/******/ 	var hotCurrentHash = "9bc079d97d8e543e5e32";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

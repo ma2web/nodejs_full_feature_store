6c8d3834ad0ce4cbb9493f42129183d797d1b45d
Mohammad Amin <mohammadazimi16@gmail.com>
2021-06-25 18:03:53 +0430

    otp

--- views/_next/static/chunks/webpack.js
+++ views/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "84bba7de718ff93f772d";
+/******/ 	var hotCurrentHash = "6d48b27d62fdbe7aead1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

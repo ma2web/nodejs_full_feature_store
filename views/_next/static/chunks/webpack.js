8a99d41520b591b3cacf746f09fabe47d60581c8
Mohammad Amin <mohammadazimi16@gmail.com>
2021-06-28 16:07:22 +0430

    with nesting dom error

--- views/_next/static/chunks/webpack.js
+++ views/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3c672ca6cb4aa47f665d";
+/******/ 	var hotCurrentHash = "ad4fea1e8a786a12ace8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
